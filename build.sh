c4358e3bc2f0d65582915c45e46e07f6c7b65828
xzmi007 <84288896+xzmi007@users.noreply.github.com>
2021-06-30 18:26:29 +0500

    Create build.sh
    Setup

---
+++ build.sh
@@ -0,0 +1,16 @@
+clear
+echo
+echo "Install All You Need, Patient Bro ..!"
+echo
+pkg update && pkg upgrade
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+pip2 install lolcat
+pkg install nodejs-lts
+pip2 install requests
+pkg install nodejs
